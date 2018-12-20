6ebd7a4b5bca0deb4151867255e639ce35dbe659
kjelldg <Kjelldg@gmail.com>
2018-12-20 20:02:45 +0100

    Added code for the user viewing arrays and sorting the lists.

--- src/main/java/com/example/YHVT19_Backend_Labb1/API_Parser.java
+++ src/main/java/com/example/YHVT19_Backend_Labb1/API_Parser.java
@@ -15,11 +15,15 @@ import okhttp3.Response;
 
 public class API_Parser {
 
+	// These static strings are used instead of inserting them below, to reduce
+	// clutter.
 	static String API_Key = "eecc8ae5b1c378032fe7a8ca2ce11da9";
 	static String TMDB_URL = "https://api.themoviedb.org/3/movie/popular?api_key=";
 	static String LANGUAGE_AND_PAGES = "&language=en-US&page=1";
 
-	public static void main(String[] args) {
+	public ArrayList<Movie> arrayListReturner() {
+
+		ArrayList<Movie> moviesArrayList = null;
 
 		TMDB_Retriever movie_Info_Retriever = new TMDB_Retriever();
 
@@ -38,26 +42,25 @@ public class API_Parser {
 			// The array for all the popular movies on page 1.
 			JSONArray moviesArray = jsonObject.getJSONArray("results");
 
-			// TODO: Add functionality for sorting.
+			moviesArrayList = movie_Info_Retriever.returnMovieArray(moviesArray);
 
-			ArrayList<Movie> moviesArrayList = movie_Info_Retriever.returnMovieArray(moviesArray);
-
-			// Returns a list of the most popular movie titles thumbnails from TMDB.
-			for (Movie movie : moviesArrayList) {
-				System.out.println("Objects from the movie array: \n" + movie.movieTitle + "\n" + movie.releaseDate);
-			}
-
-			Collections.sort(moviesArrayList, (m1, m2) -> m1.movieTitle.compareTo(m2.movieTitle));
-
-			// Returns a list of the most popular movie titles thumbnails from TMDB.
-			for (Movie movie : moviesArrayList) {
-				System.out.println(
-						"Objects from the ****NEW**** movie array: \n" + movie.movieTitle + "\n" + movie.releaseDate);
-			}
+//			// Returns a list of the most popular movie titles thumbnails from TMDB.
+//			for (Movie movie : moviesArrayList) {
+//				System.out.println("Objects from the movie array: \n" + movie.movieTitle + "\n" + movie.releaseDate);
+//			}
+//
+//			Collections.sort(moviesArrayList, (m1, m2) -> m1.movieTitle.compareTo(m2.movieTitle));
+//
+//			// Returns a list of the most popular movie titles thumbnails from TMDB.
+//			for (Movie movie : moviesArrayList) {
+//				System.out.println(
+//						"Objects from the ****NEW**** movie array: \n" + movie.movieTitle + "\n" + movie.releaseDate);
+//			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return moviesArrayList;
 
 	}
 
--- src/main/java/com/example/YHVT19_Backend_Labb1/Main.java
+++ src/main/java/com/example/YHVT19_Backend_Labb1/Main.java
@@ -1,8 +1,48 @@
 package com.example.YHVT19_Backend_Labb1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		API_Parser api_Parser = new API_Parser();
+
+		ArrayList<Movie> moviesArrayList = api_Parser.arrayListReturner();
+
+		while (true) {
+
+			System.out.println("Press 1 for viewing the most popular movies. "
+					+ "\nPress 2 for sorting the movies in alphabetical order."
+					+ "\nPress 3 for sorting movies after release date." + "\nPress 4 for reversing your movies list.");
+			int answer = Integer.parseInt(bufferedReader.readLine());
+
+			switch (answer) {
+			case 1:
+				for (Movie movie : moviesArrayList) {
+					System.out.println(movie.movieTitle + "\n" + movie.releaseDate);
+				}
+				break;
+			case 2:
+				Collections.sort(moviesArrayList, (m1, m2) -> m1.movieTitle.compareTo(m2.movieTitle));
+				break;
+			case 3:
+				Collections.sort(moviesArrayList, (m1, m2) -> m1.releaseDate.compareTo(m2.releaseDate));
+				break;
+			case 4:
+				Collections.reverse(moviesArrayList);
+				break;
+			default:
+				break;
+
+			}
+
+		}
 
 	}
 
