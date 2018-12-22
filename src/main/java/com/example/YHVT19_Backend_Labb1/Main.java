e15271accacb02222fd7dd79b080914448176c2e
kjelldg <Kjelldg@gmail.com>
2018-12-22 17:33:30 +0100

    Users can now sort movies after vote average.

--- src/main/java/com/example/YHVT19_Backend_Labb1/Main.java
+++ src/main/java/com/example/YHVT19_Backend_Labb1/Main.java
@@ -18,10 +18,11 @@ public class Main {
 		while (true) {
 
 			System.out.println("Press 1 for viewing the most popular movies. "
-					+ "\nPress 2 for sorting the movies in alphabetical order."
-					+ "\nPress 3 for sorting movies after release date."
-					+ "\nPress 4 for reversing the order of your movies list."
-					+ "\nPress 5 to search for a movie in the TMDB database.");
+					+ "\nPress 2 to search for a movie in the TMDB database."
+					+ "\nPress 3 for sorting the movies in alphabetical order."
+					+ "\nPress 4 for sorting movies after release date."
+					+ "\nPress 5 for sorting movies after vote average."
+					+ "\nPress 6 for reversing the order of your movies list.");
 			int answer = Integer.parseInt(bufferedReader.readLine());
 
 			switch (answer) {
@@ -32,21 +33,24 @@ public class Main {
 				}
 				break;
 			case 2:
-				Collections.sort(moviesArrayList, (m1, m2) -> m1.movieTitle.compareTo(m2.movieTitle));
-				break;
-			case 3:
-				Collections.sort(moviesArrayList, (m1, m2) -> m1.releaseDate.compareTo(m2.releaseDate));
-				break;
-			case 4:
-				Collections.reverse(moviesArrayList);
-				break;
-			case 5:
 				System.out.println("Please enter the name of the movie you want to search for: ");
 				ArrayList<Movie> searchedMoviesArrayList = api_Parser.searchMoviesReturner(bufferedReader.readLine());
 				for (Movie movie : searchedMoviesArrayList) {
 					System.out.println("Movie title: " + movie.movieTitle + "\nRelease date: " + movie.releaseDate
 							+ "\nVote average: " + movie.vote_average + "\n********");
 				}
+				break;
+			case 3:
+				Collections.sort(moviesArrayList, (m1, m2) -> m1.movieTitle.compareTo(m2.movieTitle));
+				break;
+			case 4:
+				Collections.sort(moviesArrayList, (m1, m2) -> m1.releaseDate.compareTo(m2.releaseDate));
+				break;
+			case 5:
+				moviesArrayList.sort((m1, m2) -> Integer.compare(m2.vote_average, m1.vote_average));
+				break;
+			case 6:
+				Collections.reverse(moviesArrayList);
 				break;
 			default:
 				break;
