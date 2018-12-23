718ba91db25154cc99167c886de5c76255db8e5a
kjelldg <Kjelldg@gmail.com>
2018-12-23 15:53:31 +0100

    Added upcoming movies.

--- src/main/java/com/example/YHVT19_Backend_Labb1/API_Parser.java
+++ src/main/java/com/example/YHVT19_Backend_Labb1/API_Parser.java
@@ -18,6 +18,7 @@ public class API_Parser {
 	// clutter.
 	static String TMDB_POPULAR_MOVIES = "https://api.themoviedb.org/3/movie/popular?api_key=";
 	static String TMDB_SEARCH_MOVIES = "https://api.themoviedb.org/3/search/movie?api_key=";
+	static String TMDB_UPCOMING_MOVIES = "\"https://api.themoviedb.org/3/movie/top_rated?api_key=";
 	static String API_Key = "eecc8ae5b1c378032fe7a8ca2ce11da9";
 	static String LANGUAGE_AND_PAGES = "&language=en-US&page=1";
 
@@ -64,6 +65,36 @@ public class API_Parser {
 		Request request = new Request.Builder().url(
 				TMDB_SEARCH_MOVIES + API_Key + "&language=en-US&query=" + userQuery + "&page=1&include_adult=false")
 				.get().build();
+
+		try {
+			Response response = client.newCall(request).execute();
+
+			String responseData = response.body().string();
+			JSONObject jsonObject = new JSONObject(responseData);
+
+			// The array for all the popular movies on page 1.
+			JSONArray moviesArray = jsonObject.getJSONArray("results");
+
+			moviesArrayList = movie_Info_Retriever.returnMovieArray(moviesArray);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return moviesArrayList;
+
+	}
+
+	public ArrayList<Movie> upcomingMoviesReturner(String userQuery) {
+
+		ArrayList<Movie> moviesArrayList = null;
+
+		TMDB_Retriever movie_Info_Retriever = new TMDB_Retriever();
+
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("application/octet-stream");
+		RequestBody body = RequestBody.create(mediaType, "{}");
+		Request request = new Request.Builder().url(TMDB_UPCOMING_MOVIES + API_Key + LANGUAGE_AND_PAGES).get().build();
 
 		try {
 			Response response = client.newCall(request).execute();
